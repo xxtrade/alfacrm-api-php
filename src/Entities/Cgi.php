b2f48d80c35b69800dad54a5b0d5592fd91ffaf6
Dmitriy Mukin <dmitry.mukin@gmail.com>
2026-02-02 22:49:45 +0300

    Add missing CGI methods and required group_id handling

--- src/Entities/Cgi.php
+++ src/Entities/Cgi.php
@@ -31,4 +31,85 @@ class Cgi extends AbstractEntity
             $filterData
         );
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function customer(int $page = 0, array $filterData = []): array
+    {
+        if (!isset($filterData['customer_id'])) {
+            throw new \ArgumentCountError(
+                'customer_id is required! $filterData = ' . json_encode($filterData, JSON_THROW_ON_ERROR)
+            );
+        }
+
+        $getParams = ['customer_id' => $filterData['customer_id']];
+
+        unset($filterData['customer_id']);
+
+        $filterData['page'] = $page;
+
+        return $this->client->sendRequest(
+            $this->preparePath(
+                "$this->modelName/customer",
+                $getParams
+            ),
+            $filterData
+        );
+    }
+
+    /**
+     * create model item
+     * @param array $entityData
+     * @return array
+     */
+    public function create(array $entityData): array
+    {
+        if (!isset($entityData['group_id'])) {
+            throw new \ArgumentCountError(
+                'group_id is required! $entityData = ' . json_encode($entityData, JSON_THROW_ON_ERROR)
+            );
+        }
+
+        $getParams = ['group_id' => $entityData['group_id']];
+
+        unset($entityData['group_id']);
+
+        return $this->client->sendRequest(
+            $this->preparePath(
+                "$this->modelName/create",
+                $getParams
+            ),
+            $entityData
+        );
+    }
+
+    /**
+     * update model item using id
+     * @param int $entityId
+     * @param array $updateData
+     * @return array
+     */
+    public function update(int $entityId, array $updateData): array
+    {
+        if (!isset($updateData['group_id'])) {
+            throw new \ArgumentCountError(
+                'group_id is required! $updateData = ' . json_encode($updateData, JSON_THROW_ON_ERROR)
+            );
+        }
+
+        $getParams = ['group_id' => $updateData['group_id']];
+        
+        $getParams['id'] = $entityId;
+
+        unset($updateData['group_id']);
+
+        return $this->client->sendRequest(
+            $this->preparePath(
+                "$this->modelName/update",
+                $getParams
+            ),
+            $updateData
+        );
+    }
 }
